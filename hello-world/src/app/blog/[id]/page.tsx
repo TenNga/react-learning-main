9f8c8ac0d15504f4ac582e05320416d7408166d5
kk4kevinkarma <kevinkarma55@gmail.com>
2024-04-08 20:23:57 -0400

    metadata for SEO

--- hello-world/src/app/blog/[id]/page.tsx
+++ hello-world/src/app/blog/[id]/page.tsx
@@ -1,6 +1,20 @@
+import { Metadata } from "next";
+
 type prepType = {
     params:{ id: string }
 }
+
+export const generateMetadata = async({params}:prepType):Promise<Metadata> => {
+    const title = await new Promise(resolve=>{
+        setTimeout(() => {
+            resolve(`number ${params.id}`)
+        }, 100);
+    })
+    return {
+        title: `Blog ${title}`,
+    };
+};
+
 function BlogPage({params}:prepType) {
     return(
         <h1>Blog Page {params.id}</h1>
