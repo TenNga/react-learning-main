8c19213c6e5d9e50e907585ea7d4c6c03174e1b7
Kevin Karma <tenngawang6080@gmail.com>
2024-04-18 11:21:28 -0400

    cookies in route handler Next cookies obj

--- route-handler-demo/src/app/profile/api/route.ts
+++ route-handler-demo/src/app/profile/api/route.ts
@@ -1,14 +1,18 @@
-import { headers } from "next/headers";
+import { headers,cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
     const requestHeaders = new Headers(request.headers);
     const headerList = headers()
 
+    cookies().set("resultsPerPage","20");
+
     console.log(requestHeaders.get("Authorization"));
     console.log(headerList.get("Authorization"));
+    
+    console.log(cookies().get("resultsPerPage"))
 
-    return new Response("<,>Profile Data here</h1>", {
+    return new Response("<h1>Profile Data here</h1>", {
         headers: {
             "Content-Type": "text/html",
         }
