674dacf14c84aa87e3c5ad4ef2ec938943ae8299
Kevin Karma <tenngawang6080@gmail.com>
2024-04-18 12:36:45 -0400

    middleware to intercept route and handle cookies, header many more

---
+++ route-handler-demo/src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const response = NextResponse.next();
+
+    const themePreference = request.cookies.get("theme");
+
+    if(!themePreference) {
+        response.cookies.set("theme", "dark");
+    }
+
+    response.headers.set("custom-header","custom-value");
+
+    return response;
+    // return NextResponse.rewrite(new URL("/hello", request.url));
+}
+
+export const config = {
+    matcher: "/profile"
+}
