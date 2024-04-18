300abc4337492e0a189938094dffa29b684f9fdf
Kevin Karma <tenngawang6080@gmail.com>
2024-04-17 20:39:50 -0400

    header handler with next/headers

--- route-handler-demo/src/app/profile/api/route.ts
+++ route-handler-demo/src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-    return new Response("Profile Data here")
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headerList = headers()
+
+    console.log(requestHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"));
+
+    return new Response("<,>Profile Data here</h1>", {
+        headers: {
+            "Content-Type": "text/html",
+        }
+    })
 }
