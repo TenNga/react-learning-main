1b41a1798a74102e218cb923d81d11ee62e9211c
Kevin Karma <tenngawang6080@gmail.com>
2024-10-04 15:18:16 -0400

    added usequery

--- query-practice/src/App.js
+++ query-practice/src/App.js
@@ -1,10 +1,32 @@
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+const POST = [
+  {id: 1, title: "Post 1"},
+  {id: 2, title: "Post 2"},
+  {id: 3, title: "Post 3"},
+]
+
 
 function App() {
+
+const postsQuery = useQuery({
+  queryKey: ["posts"],
+  queryFn: ()=> wait(1000).then(() => [...POST])
+})
+
+if(postsQuery.isLoading) return <h1>Loading....</h1>;
+if(postsQuery.isError) {
+  return <pre>{JSON.parse(postsQuery.error)}</pre>;
+}
   return (
-    <div className="App">
-      <h1>useQuery Practice</h1>
-    </div>
+      <div>
+        {postsQuery.data.map(post => <h1 key={post.id}>{post.title}</h1>)}
+      </div>
   );
+}
+
+function wait(duration) {
+  return new Promise(resolve => setTimeout(resolve, duration))
 }
 
 export default App;
