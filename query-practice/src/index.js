9f0443207a7dff289ced10bce36597b6eaaed002
Kevin Karma <tenngawang6080@gmail.com>
2024-11-03 17:44:26 -0500

    added new learning file

--- query-practice/src/index.js
+++ query-practice/src/index.js
@@ -7,7 +7,7 @@ const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClientgitgit }>
       <App />
     </QueryClientProvider>
   </React.StrictMode>
