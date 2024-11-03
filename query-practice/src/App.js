a3d853c704f040a6620ca23ceef2137e53e6c92b
Kevin Karma <tenngawang6080@gmail.com>
2024-11-03 17:49:25 -0500

    clean up query-practice file

--- query-practice/src/App.js
+++ query-practice/src/App.js
@@ -1,24 +1,10 @@
-import { useState } from "react";
-import PostsList1 from "./PostsList1";
-import PostsList2 from "./PostsList2";
+import React from "react";
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState(<PostsList1 />);
 
   return(
     <div>
-      <button onClick={() => setCurrentPage(<PostsList1 />)}>
-      Posts List 1
-      </button>
-
-      <button onClick={() => setCurrentPage(<PostsList2 />)}>
-      Posts List 2
-      </button>
-      <button>
-        First Post 
-      </button>
-      <br />
-      {currentPage}
+      <h1>App page</h1>
     </div>
   )
 };
--- query-practice/src/index.js
+++ query-practice/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClientgitgit }>
       <App />
-    </QueryClientProvider>
   </React.StrictMode>
 );
 
