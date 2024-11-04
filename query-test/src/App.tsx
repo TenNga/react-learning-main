12978947d0274203912ee7bf4318ad99ce5b0272
Kevin Karma <tenngawang6080@gmail.com>
2024-11-03 21:50:19 -0500

    added some practice usequery

--- query-test/src/App.tsx
+++ query-test/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { ProductType } from './type/product';
+import { useQueryProducts } from './hooks/useQueryProducts';
 
 function App() {
+  const [products, setProducts] = useState<ProductType[] | null>(null)
+  const {data} = useQueryProducts()
+
+  useEffect(()=>{
+    setProducts(data?.products);
+  },[])
+
+  const renderProductTitle = products?.map(pd => <li>{pd.title}</li>);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App Pager here</h1>
+      {!products && <p>Loading...</p>}
+      {products && <ol>{renderProductTitle}</ol>}
     </div>
   );
 }
---
+++ query-test/src/hooks/useQueryProducts.tsx
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios'
+import React from 'react'
+
+export const fetchProducts = async () => {
+    const URL = 'https://dummyjson.com/products';
+    const resp = await axios.get(URL)
+    const data = await resp.data
+    return data;
+}
+
+export const useQueryProducts = () => {
+  return useQuery({queryKey:['products'],queryFn:fetchProducts})
+}
