71b3f0c714aa550f18456e57cf8401c9282f1429
Kevin Karma <tenngawang6080@gmail.com>
2024-11-16 14:42:31 -0500

    style done and functionallity of status chagne with color done

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,15 +1,17 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
 
-type todoType = {
+interface todoType {
   name: string,
   status: string
 }
 
 function App() {
   const [todos, setTodos] = useState<todoType[]>([])
+  const [selectedOption, setSelectedOption] = useState<string>('')
   
   const inputRef = useRef<HTMLInputElement >(null);
+  const statusOpts = ['incomplete', 'pending', 'complete']
 
   const handleTodoSubmit = (event:React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -19,14 +21,33 @@ function App() {
         status: 'incomplete'
       }
       setTodos([...todos,newTodo])
+      inputRef.current.value = '';
     }
+  }
+
+  const handleStatusChange = (event:React.ChangeEvent<HTMLSelectElement>,todo:todoType) => {
+    let targetTodo = 0;
+    const allTodos = [...todos];
+    
+    todos.forEach((t,i) => {
+      if(t.name === todo.name)
+        targetTodo = i;
+    })
+    allTodos[targetTodo] = {name:allTodos[targetTodo].name,status: event.target.value}
+    setTodos([...allTodos])
   }
 
   const renderTodos = todos.map(todo => {
     return(
       <li key={todo.name}>
-        <input type="checkbox" />
         <label>{todo.name}</label>
+        <select value={todo.status} onChange={(event)=>handleStatusChange(event,todo)} className={`status-${todo.status}`} >
+        {statusOpts.map((option) => (
+          <option key={option} value={option} className={`status-${option}`}>
+            {option}
+          </option>
+        ))}
+      </select>
       </li>
     )
   })
@@ -36,13 +57,15 @@ function App() {
         <h1>Todo LocalStorage App</h1>
       </header>
       <main>
-        <form onSubmit={handleTodoSubmit}>
-          <input ref={inputRef} type="text" placeholder='Task' required/>
-          <button type='submit'>Add</button>
-        </form>
+        <div className='task-form'>
+          <form onSubmit={handleTodoSubmit}>
+            <input ref={inputRef} type="text" placeholder='Task' required/>
+            <button type='submit'>Add</button>
+          </form>
+        </div>
 
-        <div>
-          <ul>
+        <div className='todo-container'>
+          <ul className='todo-list'>
             {renderTodos}
           </ul>
         </div>
