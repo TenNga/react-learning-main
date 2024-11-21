9a9d89c7527b0588c7486b4554a7482a97d854e2
Kevin Karma <tenngawang6080@gmail.com>
2024-11-20 21:06:28 -0500

    added test and need practice

--- todo-app/src/components/EachTodo.tsx
+++ todo-app/src/components/EachTodo.tsx
@@ -3,11 +3,11 @@ import { todoType } from '../types'
 import { statusOpts } from '../constant'
 
 
-const EachTodo = ({todo,handleStatusChange}: {todo:todoType,handleStatusChange: (event: React.ChangeEvent<HTMLSelectElement>, todo: todoType) => void}) => {
+const EachTodo = ({todo,handleStatusChange}: {todo:todoType,handleStatusChange?: (event: React.ChangeEvent<HTMLSelectElement>, todo: todoType) => void}) => {
   return (
     <li key={todo.name}>
-        <label>{todo.name}</label>
-        <select value={todo.status} onChange={(event)=>handleStatusChange(event,todo)} className={`status-${todo.status}`} >
+        <label data-testid="todoName">{todo.name}</label>
+        <select value={todo.status} onChange={(event)=>handleStatusChange && handleStatusChange(event,todo)} className={`status-${todo.status}`} >
         {statusOpts.map((option) => (
           <option key={option} value={option} className={`status-${option}`}>
             {option}
